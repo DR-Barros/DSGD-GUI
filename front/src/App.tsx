1183e6115d6476bb2f74ad079958309a15260375
Daniel Radrigán <danielradriganb@gmail.com>
2025-08-24 19:00:20 -0400

    REFACTOR(Experiments): se mueve de carpeta experiments

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
-import Experiments from "./pages/experiment/Experiments";
+import Experiments from "./pages/experiments/Experiments";
 import Datasets from "./pages/dataset/Datasets";
 import UploadDatasets from "./pages/dataset/UploadDatasets";
 import "./i18n";
