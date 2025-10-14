66276f949da8d3cf1ddcd7e39adbbc6b35979f4f
Daniel Radrigán <danielradriganb@gmail.com>
2025-10-13 23:52:27 -0300

    FEAT(tests): se agrega test a de registro y login

---
+++ back/tests/auth/test_post_register_login.py
@@ -0,0 +1,17 @@
+def test_register_and_login_and_refresh(client):
+    # Register
+    payload = {"name": "Test", "email": "t@example.com", "password": "secret"}
+    r = client.post("/dsgd/api/auth/register", json=payload)
+    assert r.status_code == 200
+    data = r.json()
+    assert data["email"] == "t@example.com"
+    
+    res = client.post("/dsgd/api/auth/register", json=payload)
+    assert res.status_code == 400
+    assert res.json()["detail"] == "Email ya registrado"
+    
+    #login
+    r = client.post("/dsgd/api/auth/login", data={"username": "t@example.com", "password": "secret"})
+    assert r.status_code == 200
+    assert "access_token" in r.cookies
+    assert "refresh_token" in r.cookies
