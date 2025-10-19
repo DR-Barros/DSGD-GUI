f7973ed3fbd9d7d1e4782f40c6edce1944971e8f
Daniel Radrigán <danielradriganb@gmail.com>
2025-10-19 10:26:38 -0300

    FEAT(jwt_handler): se agrega validacion del tipo de token

--- back/auth/jwt_handler.py
+++ back/auth/jwt_handler.py
@@ -11,17 +11,21 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
+    to_encode.update({"type": "access"})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 def create_refresh_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
+    to_encode.update({"type": "refresh"})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 def decode_access_token(token: str):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        if payload.get("type") != "access":
+            return None
         return payload
     except JWTError:
         return None
